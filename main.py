098244aca52fadfa06cb21b959c0c243270874df
Fred Wang <namelos@outlook.com>
2016-11-01 23:58:14 +0800

    add map

--- main.py
+++ main.py
@@ -5,13 +5,27 @@ class Container(object):
 def mapContainer(f):
     return lambda container: f(container.val)
 
-
-
 """
 Maybe = Some(val) | None
 """
 
+"""
+Some value
+"""
 class Some(object):
     def __init__(self, val):
         self.val = val
 
+    def __str__(self):
+        return "Some %s" % self.val
+
+"""
+map_maybe :: (a -> b) -> Maybe a -> Maybe b
+"""
+def map_maybe(f):
+    def apply(maybe):
+        if maybe is None:
+            return None
+        else:
+            return Some(f(maybe.val))
+    return apply
